1815ca56bc22f594dd3a1d9450af04fe1f9f594a
fillup00 <jt885314@ohio.edu>
2018-07-11 17:07:06 -0400

    Commit with working authentication and account creation.

---
+++ src/components/Landing/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const LandingPage = () =>
+  <div>
+    <div>
+      <h1>Debate Thee</h1>
+      <p>
+        Welcome to Debate Thee! This website was built in order to help close the gap of radical disagreements. We need to have a logical debate with facts in order to prove your point.
+        Factual arguments are the easiest way to get your point across.
+      </p>
+      <p>
+        Hi my name is Jonathan Telep. I graduated from Ohio University in 2018 with a Bachelors Degree in Computer Science. During my collegiate career, I lived through arguable the most
+        political turmoil presidential election of 2016. I heard and saw families get in to heated arguments about which candidate is better. Families were separated and friendships were
+        broken on the regards of support of a presidential candidate. Debating is always the best way to inform a person your beliefs and what should happen with our government. Agreeing
+        to disagree also is very important. Respecting the other's choice and seeing where they come from.
+      </p>
+    </div>
+  </div>
+
+export default LandingPage;
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,21 +1,26 @@
-import * as firebase from 'firebase';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+//DebateThee
 const prodConfig = {
-    apiKey: "AIzaSyBL5kPJ0jM7HGIlk4E-iV3OCBgA6iKekFg",
-    authDomain: "debatethee.firebaseapp.com",
-    databaseURL: "https://debatethee.firebaseio.com",
-    projectId: "debatethee",
-    storageBucket: "debatethee.appspot.com",
-    messagingSenderId: "35166371028"
+  apiKey: "AIzaSyBL5kPJ0jM7HGIlk4E-iV3OCBgA6iKekFg",
+  authDomain: "debatethee.firebaseapp.com",
+  databaseURL: "https://debatethee.firebaseio.com",
+  projectId: "debatethee",
+  storageBucket: "debatethee.appspot.com",
+  messagingSenderId: "35166371028"
 };
 
-const devConfig = {
-    apiKey: "AIzaSyBL5kPJ0jM7HGIlk4E-iV3OCBgA6iKekFg",
-    authDomain: "debatethee.firebaseapp.com",
-    databaseURL: "https://debatethee.firebaseio.com",
-    projectId: "debatethee",
-    storageBucket: "debatethee.appspot.com",
-    messagingSenderId: "35166371028"
+//DevDebateThee
+const devConfig = {  
+  apiKey: "AIzaSyDEUKYiLM3Sn7W3WSAmEAXwx08RCWpM3SI",
+  authDomain: "devdebatethee.firebaseapp.com",
+  databaseURL: "https://devdebatethee.firebaseio.com",
+  projectId: "devdebatethee",
+  storageBucket: "devdebatethee.appspot.com",
+  messagingSenderId: "252041547941"
 };
 
 const config = process.env.NODE_ENV === 'production'
@@ -26,8 +31,10 @@ if (!firebase.apps.length) {
   firebase.initializeApp(config);
 }
 
+const db = firebase.database();
 const auth = firebase.auth();
 
 export {
+  db,
   auth,
 };
